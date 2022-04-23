df89cce1c1ecade152a5d48eb9e1dd191de60f15
alphastation <alexander111111@gmail.com>
2022-04-23 23:10:25 +0100

    node backend and express framework added;fetching initial posts and using angular http client; POST backend-point added; relevant changes to connect angular to backend made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -31,6 +32,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatCardModule,
     MatToolbarModule,
     MatExpansionModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
