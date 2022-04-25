1fe25c81508b5ab87ec65c349d91f006745fb3e0
alphastation <alexander111111@gmail.com>
2022-04-25 17:26:49 +0100

    implementation of EDIT functionality started; the old post-create compo is being used for this new feature with the help of paramMap observable of ActivatedRoute; new GET single POST service added in post.service.ts; tsconfig.json modified again with strictNullChecks set to false;

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,23 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
-
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css'],
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
   // newPost = 'No content whatsoever';
   // enteredValue = '';
   contentEntered = '';
   titleEntered = '';
+  private mode = 'create';
+  private postId: string;
+  private post: Post;
   // @Output() postCreatedEvent = new EventEmitter<Post>();
   // postInput: HTMLTextAreaElement
-  constructor(public postsService: PostsService) {
+  constructor(public postsService: PostsService, public route: ActivatedRoute) {
     // this.postsService = postsService;
+  }
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        // this.postId = paramMap.get('postId') || '';
+        this.post = this.postsService.getPost(this.postId);
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
   }
   onCreatingPost(form: NgForm) {
     if (form.invalid) {
